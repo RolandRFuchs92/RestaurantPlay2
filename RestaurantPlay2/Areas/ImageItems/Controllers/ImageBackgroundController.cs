fc8b84602b41e06ceb7daf01dd2c4733e8835d55
RolandRFuchs92 <Roland@DESKTOP-FA4U2TR>
2018-05-08 07:49:33 +0200

    Added Background Image Stuff

--- RestaurantPlay2/Areas/ImageItems/Controllers/ImageBackgroundController.cs
+++ RestaurantPlay2/Areas/ImageItems/Controllers/ImageBackgroundController.cs
@@ -8,6 +8,10 @@ namespace RestaurantPlay2.Areas.ImageItems.Controllers
 {
     public class ImageBackgroundController : Controller
     {
-
+        private readonly int _imageTypeID = 3;
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
---
+++ RestaurantPlay2/Areas/ImageItems/ViewModels/BackgroundImage/SaveBackgroundImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantPlay2.Areas.ImageItems.ViewModels.BackgroundImage
+{
+    public class SaveBackgroundImage
+    {
+        public HttpFileCollectionBase image { get; set; }
+        public int priority { get; set; }
+    }
+}
