0d1fc45d54ce738a7e6fe6bdab1e402075cb494e
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-05-16 21:36:20 +0200

    Added Initial Entities and start to the repo.

---
+++ DataAccess/Entities/BlogDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class BlogDetail
+    {
+        [Key]
+        public int BlogDetailId { get; set; }
+        public string BlogImagePath { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public DateTime BlogWritenOn { get; set; }
+        public string BlogIntro { get; set; }
+        public string BlogBody { get; set; }
+        public string BlogRemarks { get; set; }
+        public string BlogClosingCaption { get; set; }
+        public DateTime BlogStartingDate { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ DataAccess/Entities/COREBlog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class COREBlog
+    {
+        [Key]
+        public int COREBlogID { get; set; }
+        public int BLOGTypeID { get; set; }
+        public int BLOGDetailID { get; set; }
+    }
+}
