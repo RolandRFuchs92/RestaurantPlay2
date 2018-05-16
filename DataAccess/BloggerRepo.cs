671c5c1697b00d1fb2e53661e9ce0b7283ca16c4
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-05-16 21:50:27 +0200

    Added some logic to the blogger to perform basic read functions

--- DataAccess/BloggerRepo.cs
+++ DataAccess/BloggerRepo.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Context;
+using DataAccess.Entities;
 
 namespace DataAccess
 {
     public class BloggerRepo
     {
+        private AppsContext _db => new AppsContext();
+
+        public List<BlogDetail> GetAllBlogDetails()
+        {
+            return (from blog in _db.BlogDetails
+                    select blog).ToList();
+        }
+
+        public BlogDetail GetLatestBlog()
+        {
+            return (from blog in _db.BlogDetails
+                    orderby blog.BlogStartingDate
+                    select blog).FirstOrDefault();
+        }
+
+        public BlogDetail GetBlogDetailById(int blogId)
+        {
+            return (from blog in _db.BlogDetails
+                    where blog.BlogDetailId == blogId
+                    select blog).FirstOrDefault();
+        }
     }
 }
