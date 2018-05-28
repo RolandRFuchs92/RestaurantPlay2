15be58b2ebcf2af1c275265489688fbf21a1a18e
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-05-28 20:13:04 +0200

    Added Booking Entity

---
+++ DataAccess/Entities/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+		public class Booking
+		{
+			public int BookingId { get; set; }
+			public string BookingMobileNumber { get; set; }
+			public string BookingEmailAddress { get; set; }
+			public int BookingHeadCount { get; set; }
+			public string BookingComments { get; set; }
+			public int BookingOccassion { get; set; }
+			public bool IsConfirmed { get; set; }
+			public bool IsCanceled { get; set; }
+		}
+}
