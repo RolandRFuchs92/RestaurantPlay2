f55e9470f5b6d7fff3109306a51b4412a5e3462c
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-05-28 21:30:50 +0200

    added an entity and there are many errors, feeling tired, will carry on tomorrow.

--- RestaurantPlay2/Areas/BookNow/BusinessLogic/BookNow.cs
+++ RestaurantPlay2/Areas/BookNow/BusinessLogic/BookNow.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DataAccess.Context;
+using RestaurantPlay2.Areas.BookNow.ViewModels;
 
 namespace RestaurantPlay2.Areas.BookNow.BusinessLogic
 {
     public class BookNow
     {
+        public List<ListCategoryViewModel> GetListCategoryViewModel()
+        {
+            
+
+            return new AppsContext().ItemLists.Where(i => i.ListCategoryId == 1).ToList();
+        }
     }
 }
