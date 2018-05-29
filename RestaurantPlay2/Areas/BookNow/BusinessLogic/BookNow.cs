4d4eed81db4ae0e7c4e055d4c50b627bd0566c02
RolandRFuchs92 <Roland@DESKTOP-FA4U2TR>
2018-05-29 08:45:16 +0200

    Adjusted the Occasion on BooknowViewmodel, added logic to return a model on businesslogic.

--- RestaurantPlay2/Areas/BookNow/BusinessLogic/BookNow.cs
+++ RestaurantPlay2/Areas/BookNow/BusinessLogic/BookNow.cs
@@ -24,5 +24,32 @@ namespace RestaurantPlay2.Areas.BookNow.BusinessLogic
 
 						return model;
 				}
+
+				private List<BookNowViewModel> GetTodaysBookings()
+				{
+						var db = new AppsContext();
+						var bookingRepo = new BookingRepo(db);
+						var ItemListRepo = new ItemListRepo(db);
+
+						var model = (from booking in bookingRepo.GetTodaysBookings()
+												 join occasion in ItemListRepo.GetListItemsByCateogry(1) on booking.BookingOccassionId equals occasion.ListId
+												 select new BookNowViewModel
+												 {
+														 Comments = booking.BookingComments,
+														 CreatedOn = booking.CreateOn,
+														 EmailAddress = booking.BookingEmailAddress,
+														 ForDate = booking.BookingDate,
+														 HeadCount = booking.BookingHeadCount,
+														 IsCanceled = booking.IsCanceled,
+														 Id = booking.BookingId,
+														 IsConfirmed = booking.IsConfirmed,
+														 MobileNumber = booking.BookingMobileNumber,
+														 Name = booking.Name,
+														 Occasion = occasion.ListData
+												 }).ToList();
+
+						return model;
+				}
+
 		}
 }
---
+++ RestaurantPlay2/Areas/BookNow/ViewModels/BookNowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantPlay2.Areas.BookNow.ViewModels
+{
+		public class BookNowViewModel
+		{
+				public int Id { get; set; }
+				public string  Name { get; set; }
+				public string MobileNumber { get; set; }
+				public string EmailAddress { get; set; }
+				public int HeadCount { get; set; }
+				public string Comments { get; set; }
+				public string Occasion { get; set; }
+				public bool IsConfirmed { get; set; }
+				public bool IsCanceled { get; set; }
+				public DateTime ForDate { get; set; }
+				public DateTime CreatedOn { get; set; }
+		}
+}
