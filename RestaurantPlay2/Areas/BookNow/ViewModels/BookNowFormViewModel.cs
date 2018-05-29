1c30174950a449675c2b2565aae7d991f64ec7ae
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-05-29 20:52:33 +0200

    Changed the cshtml for booking, add savebookingviewmodel to booknowformviewmodel

--- RestaurantPlay2/Areas/BookNow/ViewModels/BookNowFormViewModel.cs
+++ RestaurantPlay2/Areas/BookNow/ViewModels/BookNowFormViewModel.cs
@@ -9,26 +9,7 @@ namespace RestaurantPlay2.Areas.BookNow.ViewModels
 {
 		public class BookNowFormViewModel
 		{
-				public BookNowFormViewModel()
-				{
-						BookingId = 0;
-				}
-				public int BookingId { get; set; }
-				[MaxLength(50)]
-				[Required]
-				public string Name { get; set; }
-				[MaxLength(15)]
-				[Required]
-				public string MobileNumber { get; set; }
-				[MaxLength(200)]
-				public string EmailAddress { get; set; }
-				[Required]
-				public int HeadCount { get; set; }
-				[MaxLength(250)]
-				public string Comment { get; set; }
-				[Required]
-				public int OccasionId { get; set; }
-				[Required]
-				public bool IsConfirmed { get; set; }
+			public SaveBookingViewModel SaveBookingViewModel { get; set; }
+			public ListCategoryViewModel ListCategoryViewModel { get; set; }
 		}
 }
---
+++ RestaurantPlay2/Areas/BookNow/ViewModels/SaveBookingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantPlay2.Areas.BookNow.ViewModels
+{
+		public class SaveBookingViewModel
+		{
+			public SaveBookingViewModel()
+			{
+				BookingId = 0;
+			}
+
+			public int BookingId { get; set; }
+			[MaxLength(50)]
+			[Required]
+			public string Name { get; set; }
+			[MaxLength(15)]
+			[Required]
+			public string MobileNumber { get; set; }
+			[MaxLength(200)]
+			public string EmailAddress { get; set; }
+			[Required]
+			public int HeadCount { get; set; }
+			[MaxLength(250)]
+			public string Comment { get; set; }
+			[Required]
+			public int OccasionId { get; set; }
+			[Required]
+			public bool IsConfirmed { get; set; }
+		}
+}
